8d96e371a39f2064856d9266fe2dad179e8f8bbc
xiaoha <blairjade183@gmail.com>
2024-06-19 12:09:56 +0100

    Merge branch 'master' of https://github.com/Simple-Recipes/backend
    add aop

--- Recipes-server/target/generated-sources/annotations/com/recipes/mapper/UserMapperImpl.java
+++ Recipes-server/target/generated-sources/annotations/com/recipes/mapper/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-15T09:45:15+0100",
+    date = "2024-06-19T10:52:11+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
